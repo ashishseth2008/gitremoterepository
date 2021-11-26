b16a1eb1ac96f5a25146acf3991c610a6ca59812
ashishseth2008 <ashishseth2008@gmail.com>
2021-11-26 13:31:46 +0530

    credentials

---
+++ main.py
@@ -0,0 +1,12 @@
+def feature1():
+    pass
+
+def log(message):
+    pass
+def user_profile_pic():
+    pass
+def reset_password():
+    pass
+def set_usr_credaintials(name,password):
+    pass
+    
